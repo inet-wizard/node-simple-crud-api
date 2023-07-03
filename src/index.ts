a4c51a24f3219c90c5705224e335e099055f10fd
Net Wizard <internet.wizard@icloud.com>
2023-07-03 18:05:32 +0300

    feat: connect db to the App

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import dotenv from 'dotenv';
+import DB from './libs/db';
 import App from './app';
+import { configDB } from './libs/dbHelpers';
 import { PORT } from './utils/constants';
 
 dotenv.config();
@@ -8,7 +10,8 @@ dotenv.config();
 const server = http.createServer();
 
 const requestHandler = async (req: IncomingMessage, res: ServerResponse) => {
-  const app = new App();
+  const db = new DB();
+  const app = new App(db);
   await app.requestHandler(req, res);
 };
 
@@ -20,12 +23,15 @@ const startServer = () => {
 };
 
 try {
+  configDB.start();
   startServer();
   process.on('SIGINT', async () => {
+    configDB.end();
     process.exit();
   });
 } catch (error: unknown) {
   const err = error as Error;
   process.stderr.write(`App error - ${err.message}`);
+  configDB.end();
   process.exit(1);
 }
